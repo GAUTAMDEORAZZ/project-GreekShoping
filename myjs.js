15f24da7521ab3b07384531218b508093b0930d3
Abdul Rub <arub0419@gmail.com>
2022-05-08 19:55:03 +0530

    Products are added

---
+++ myjs.js
@@ -0,0 +1,24 @@
+
+    var count= localStorage.getItem("counterValue") || 0;
+    if (localStorage.getItem("counterValue")==null){
+        count=0
+    }else{
+       count= localStorage.getItem("counterValue")
+    }
+    document.querySelector("h1").innerText =count;
+    document.querySelector("#Inc").addEventListener("click",incfun);
+
+    function incfun(){
+        count++
+        document.querySelector("h1").innerText=count;
+        localStorage.setItem("counterValue", count);
+    }
+
+    document.querySelector("#Dec").addEventListener("click",decfun);
+
+function decfun(){
+    count--
+    document.querySelector("h1").innerText=count;
+    localStorage.setItem("counterValue", count);
+}
+
